528d6e2583455a00810e134fcf750554621d8351
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-07-17 22:22:58 -0400

    adds testing for app.js file

--- src/app.js
+++ src/app.js
@@ -4,16 +4,7 @@ import './app.css'
 import styles from './styles.module.css'
 import 'font-awesome/css/font-awesome.css'
 
-const App = React.createClass({
-	render: function() {
-		return (
-			<div className={styles.wrapper}>
-				<i className="fa fa-star"></i>
-				<h1>Environment: {__NODE_ENV__}</h1>
-			</div>
-		);
-	}
-});
+import App from 'containers/App/App'
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(<App />, mountNode);
