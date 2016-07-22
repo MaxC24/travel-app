9f2f8f7d2bbabbf799f89414f565823781f1bb09
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-07-21 21:40:40 -0400

    adds component Router

--- src/app.js
+++ src/app.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom'
 import './app.css'
 import styles from './styles.module.css'
 import 'font-awesome/css/font-awesome.css'
-
 import App from 'containers/App/App'
 
+import {browserHistory, Router, Route} from 'react-router'
+
+const Home = React.createClass({
+	render: function() {
+		return (<div>Hello World</div>)
+	}
+})
+
+const routes = (
+	<Router>
+		<Route path="/" component={Home} />
+	</Router>
+)
+
+
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+	<App history={browserHistory} routes={routes}/>, mountNode);
