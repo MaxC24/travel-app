5053f9632d704486a0801c7644baeeb510b7de8e
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-07-23 11:28:59 -0400

    adds input fields to the app

---
+++ src/components/inputs.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Inputs extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Location</h3>
+				<input />
+				<h3>dates</h3>
+				<input />
+				<h3>Budget</h3>
+				<input />
+			</div>
+		);
+	}
+}
+
+export default Inputs;
