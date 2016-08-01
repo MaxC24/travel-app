f947b87e29f139e7c50bafb1d5e7b9b01638a089
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-08-01 19:06:15 -0400

    adds a button for submission

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Inputs extends Component {
+
+	findDestination() {
+		return alert('You pressed me');
+	}
+
 	render() {
 		return (
 			<div>
@@ -10,6 +15,7 @@ class Inputs extends Component {
 				<input />
 				<h3>Budget</h3>
 				<input />
+				<button onClick={ this.findDestination.bind(this) } >Submit</button>
 			</div>
 		);
 	}
